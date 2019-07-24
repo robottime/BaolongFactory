59167f054b89e4ceff51db9d015f9ca22676bcf7
SONG-LAPTOP\song <zhaosongy@126.com>
2019-07-24 14:13:12 +0800

    init vue project with vue-router, bootstrap-vue and fontawesome

---
+++ baolong-factory/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+
+import './plugins/bootstrap-vue';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import App from './App.vue';
+import router from './router';
+
+library.add(faUserSecret);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app');
